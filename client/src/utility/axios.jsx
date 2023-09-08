d18f1410e9d21680f3740ca3fe55ad97341422b4
Yesuf2023 <134982447+Yesuf2023@users.noreply.github.com>
2023-09-07 22:14:43 -0500

    new URL for axios

--- client/src/utility/axios.jsx
+++ client/src/utility/axios.jsx
@@ -1,10 +1,12 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:4500", // api url
+  baseURL: "https://blue-violet-crocodile-toga.cyclic.cloud",
+  // baseURL: "http://localhost:4500", // api url
+  
   // baseURL: "https://clean-gloves-bee.cyclic.app", // api url
   // baseURL: "https://vast-plum-scallop-slip.cyclic.app",
-
+  
   withCredentials: true,
 });
 
